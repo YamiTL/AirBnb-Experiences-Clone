98b7dfa5e7d0f2a7df4b711f42bbfd5877c28a72
YamiTL <84737730+YamiTL@users.noreply.github.com>
2022-03-12 12:13:28 -0300

    First Commit

---
+++ components/Card.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Card() {
+    return (
+    <div className="card">
+        <img src="../images/katiezaferes.png" className= "card--image" />
+        <div className="card--stats">
+            <img src="../images.star.png" />
+            <span>5.0</span>
+            <span className="gray">USA</span>
+            <span className="gray">(6)</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136</span></p>
+            </div>
+    )
+}
